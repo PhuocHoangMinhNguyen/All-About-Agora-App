7d8c4ee6b6c4c7995aa12824cde9c49faf3a922c
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-11-16 11:20:38 +1100

    Update (16/11)

--- src/screens/Profile/EditScreen.js
+++ src/screens/Profile/EditScreen.js
@@ -1,57 +1,42 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import auth from "@react-native-firebase/auth";
-import firestore from "@react-native-firebase/firestore";
-import Education from './Education';
-import Summary from './Summary';
-import Background from "../../components/Background";
+import React from "react";
+import { Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import Summary from "./Summary";
+import Education from "./Education";
 
-class EditScreen extends React.Component {
-    state = {
-        user: {
-            name: "Add your name",
-            address: "Add your location"
-        }
-    }
-
-    componentDidMount = async () => {
-        const user = this.props.uid || (auth().currentUser || {}).uid
-
-        const firestoreUser = await firestore().collection("users").doc(user).get();
-        if (firestoreUser.exists) {
-            this.setState({ user: firestoreUser.data() });
-        }
-    }
-
-    render() {
-        const { user } = this.state
-        return (
-            <View>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("EditProfile")}>
-                    <Background />
-                    <View style={{ alignSelf: "center", position: "absolute", marginTop: 50 }}>
-                        <Text style={styles.name}>{user.name}</Text>
-                        <Text style={styles.address}>{user.address}</Text>
-                    </View>
-                </TouchableOpacity>
-                <Summary />
-                <Education />
-            </View>
-        )
-    }
-};
-
-const styles = StyleSheet.create({
-    name: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize: 18,
-        textAlign: "center"
+const EditScreen = createStackNavigator(
+    {
+        default: createMaterialTopTabNavigator(
+            {
+                Summary: {
+                    screen: Summary,
+                    navigationOptions: {
+                        tabBarLabel: ({ }) => (
+                            <Text>Summary</Text>
+                        ),
+                    },
+                },
+                Education: {
+                    screen: Education,
+                    navigationOptions: {
+                        tabBarLabel: ({ }) => (
+                            <Text>Education</Text>
+                        ),
+                    },
+                },
+            },
+            {
+                tabBarOptions: {
+                    scrollEnabled: false,
+                },
+            }
+        ),
     },
-    address: {
-        color: "white",
-        textAlign: "center"
+    {
+        mode: "modal",
+        headerMode: "none",
     }
-});
+);
 
 export default EditScreen
