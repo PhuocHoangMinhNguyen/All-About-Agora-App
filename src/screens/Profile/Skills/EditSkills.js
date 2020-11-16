05d28be2b953ac8bf683928088c3be5c55fb40f9
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-11-16 18:51:55 +1100

    Update (16/11)

--- src/screens/Profile/Skills/EditSkills.js
+++ src/screens/Profile/Skills/EditSkills.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, FlatList } from 'react-native';
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 class EditSkills extends React.Component {
+    state = {
+        skills: ['aa'],
+        addingSkill: ''
+    }
+
+    addSkill = () => {
+        const { addingSkill, skills } = this.state
+        if (addingSkill.trim() != '') {
+            const tempArray = skills
+            tempArray.push(addingSkill.trim())
+            this.setState({
+                addingSkill: '',
+                skills: tempArray
+            });
+        }
+    }
+
+    deleteSkill(item) {
+        console.log(item)
+        const { skills } = this.state
+        const tempArray = skills
+        tempArray.forEach((temp, i) => {
+            if (item == temp) {
+                tempArray.splice(i, 1)
+            }
+        })
+        this.setState({ skills: tempArray })
+    }
+
+    renderItem = (item) => {
+        return (
+            <View style={styles.item}>
+                <Text style={{ color: 'white', alignSelf: "center", flex: 1, textAlign: "center", marginHorizontal: 10 }}>{item}</Text>
+                <TouchableOpacity style={{ alignSelf: "center", marginRight: 10 }} onPress={() => this.deleteSkill(item)}>
+                    <Ionicons name="close" size={20} color="white" />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -15,6 +55,20 @@ class EditSkills extends React.Component {
                         <Ionicons name="save-sharp" size={32} />
                     </TouchableOpacity>
                 </View>
+                <View style={{ marginHorizontal: 20, flexDirection: "row" }}>
+                    <TextInput style={styles.input}
+                        placeholder="Add skill"
+                        onChangeText={addingSkill => this.setState({ addingSkill: addingSkill })}
+                        value={this.state.addingSkill} />
+                    <TouchableOpacity onPress={this.addSkill}>
+                        <Ionicons name="add" size={32} />
+                    </TouchableOpacity>
+                </View>
+                <FlatList style={styles.list}
+                    data={this.state.skills}
+                    renderItem={({ item }) => this.renderItem(item)}
+                    horizontal={true}
+                    keyExtractor={(item) => item.id} />
             </View>
         )
     }
@@ -34,6 +88,25 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         alignSelf: 'center',
         fontSize: 24
+    },
+    input: {
+        borderBottomColor: "#8A8F9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#161F3D",
+        flex: 1
+    },
+    list: {
+        padding: 15,
+    },
+    item: {
+        minWidth: 100,
+        height: 40,
+        borderRadius: 20,
+        flexDirection: "row",
+        backgroundColor: "#003787",
+        marginHorizontal: 5
     }
 });
 
