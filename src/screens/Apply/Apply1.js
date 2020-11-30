76d4ff8d715f843d1a78f8daba442b35bfab4b7a
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-11-30 18:39:45 +1100

    Update (30/11)

--- src/screens/Apply/Apply1.js
+++ src/screens/Apply/Apply1.js
@@ -1,16 +1,73 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 
 class Apply1 extends React.Component {
+    handleUploadResume = () => {
+
+    };
+
+    handleUploadCoverLetter = () => {
+
+    };
+
     render() {
         return (
-            <Text>Apply 1</Text>
-        )
-    }
+            <View style={styles.container}>
+                <ScrollView>
+                    <Text style={styles.title}>Documents for this application</Text>
+                    <Text style={styles.intro}>{`You can choose to add a cover letter and a resume to your application.`}</Text>
+                    <View style={styles.header}>
+                        <Text style={styles.resume}>Resume</Text>
+                        <TouchableOpacity onPress={this.handleUploadResume}>
+                            <Text style={styles.upload}>Upload</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.header}>
+                        <Text style={styles.resume}>Cover letter</Text>
+                        <TouchableOpacity onPress={this.handleUploadCoverLetter}>
+                            <Text style={styles.upload}>Upload</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+                <TouchableOpacity style={styles.continue} onPress={() => this.props.navigation.navigate("Apply2")}>
+                    <Text style={{ color: 'white' }}>Continue</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    };
 };
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        padding: 20
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    intro: {
+        marginBottom: 20
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    resume: {
+        fontWeight: 'bold'
+    },
+    upload: {
+        color: 'blue'
+    },
+    continue: {
+        alignItems: "center",
+        justifyContent: 'center',
+        height: 40,
+        backgroundColor: "#001F4C",
+        borderRadius: 10,
+    },
 });
 
 export default Apply1;
--- src/screens/Apply/Apply2.js
+++ src/screens/Apply/Apply2.js
@@ -5,8 +5,8 @@ class Apply2 extends React.Component {
     render() {
         return (
             <Text>Apply 2</Text>
-        )
-    }
+        );
+    };
 };
 
 const styles = StyleSheet.create({
